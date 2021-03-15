0c40debf9d81ac24cdba83cdaef1dfecc23e59a7
VelOuria <sofleos@gmail.com>
2021-03-15 17:59:23 -0600

    added image

--- column/sketch.js
+++ column/sketch.js
@@ -1,7 +1,37 @@
+let rectangles = [];
+let spine;
+let rectHeight = 300;
+let rectWidth = 100;
+let colorR = 0.0;
+let colorG = 0.0;
+let colorB = 0.0;
+let noiseX = 0.0;
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
+
+  rectMode(CENTER);
+  imageMode(CENTER);
+  y = height;
+  x = width;
+  for (let i = 0; i < 9; i++) {
+    y = y + rectHeight;
+    rectangles.push(new Rectangles(x, y, rectWidth));
+  }
+  spine = loadImage("./assets/columna.jpeg");
 }
 
 function draw() {
-
+  background(0);
+  //tint(255, 100);
+  image(spine, width / 2, height / 2, width + 600, height);
+  for (i = 0; i < rectangles.length; i++) {
+    rectangles[i].move();
+    rectangles[i].changeColor();
+    rectangles[i].fractalize(
+      x / 2,
+      rectangles[i].y + 75,
+      rectWidth,
+      rectHeight
+    );
+  }
 }
